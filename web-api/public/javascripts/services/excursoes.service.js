914226b66f672eba7a37b76d3023807a3cfc2de4
leggen-assis <souzaferreira437@gmail.com>
2025-07-17 08:25:55 -0300

    feat: classe de serviços para excursões adicionada

---
+++ web-api/public/javascripts/services/excursoes.service.js
@@ -0,0 +1,11 @@
+const ExcursaoModel = require("../models/excursao.model")
+const excursoes = require("../data/excursoes.js")
+
+class ExcursaoService {
+    static recuperarExcursao(id) {
+        for(let i = 0; i < excursoes.length; ++i) {
+            if(excursoes[i].id == id) return excursoes[i]
+        }
+        return null
+    }
+}
