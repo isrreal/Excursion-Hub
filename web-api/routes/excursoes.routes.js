c3fe921ef4bc8612095aef17845a7cec5a9a1404
leggen-assis <souzaferreira437@gmail.com>
2025-07-17 11:10:37 -0300

    fix: caminho para 'routes' corrigido

--- web-api/routes/excursoes.routes.js
+++ web-api/routes/excursoes.routes.js
@@ -1,14 +1,18 @@
-const express = require("express")
-const ExcursaoService = require("../public/javascripts/services/excursoes.service")
-const router = express.Router()
-
+const express = require("express");
+const path = require('path');
+const ExcursaoService = require('../public/javascripts/services/excursoes.service');
+const router = express.Router();
 
 router.get(
-    "/excursoes/:id", 
+    "/excursoes/:id",
     (request, response) => {
-        const excursao = ExcursaoService.recuperarExcursao(request.params.id)
-        response.json(excursao)
+        const excursao = ExcursaoService.recuperarExcursao(parseInt(request.params.id));
+        if (excursao) {
+            response.json(excursao);
+        } else {
+            response.status(404).json({ message: "Excursão não encontrada" });
+        }
     }
-)
+);
 
-module.exports = router
+module.exports = router;
