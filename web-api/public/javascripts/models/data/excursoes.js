c9b61104481e07ee12c903087c7d68f11f204c71
leggen-assis <souzaferreira437@gmail.com>
2025-07-17 08:10:43 -0300

    feat: data info added

---
+++ web-api/public/javascripts/models/data/excursoes.js
@@ -0,0 +1,13 @@
+const excursoes = [
+    {
+        localDeViagem: "Porto das Dunas",
+        organizador: "Ana Silva",
+        quantidadeDeParticipantes: 12,
+        data: "15 de julho de 2024",
+        destino: "Porto das Dunas, Aquiraz",
+        descricao: `Aproveite um dia incrível no Porto das Dunas, um dos destinos mais famosos do litoral cearense,
+                    localizado a poucos minutos de Fortaleza. Com suas praias extensas, 
+                    dunas douradas e o famoso parque aquático Beach Park, a região oferece diversão,
+                    descanso e paisagens de tirar o fôlego.`
+    }
+]
